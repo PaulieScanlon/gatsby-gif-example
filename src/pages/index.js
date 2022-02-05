217f751d6942b61bbfd20f10836efd20f40ef17c
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-02-05 14:35:35 -0500

    chore: initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import lemon from '../gifs/lemon.gif';
+
+const Page = () => {
+  return (
+    <div>
+      <img src={lemon} alt="A Lemon" />
+    </div>
+  );
+};
+
+export default Page;
